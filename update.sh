6fcb5a555a8c9c0debd91de7b8c84a34ab481f8a
Skiddle Git Assistant <160487326+Skiddle-Git@users.noreply.github.com>
2024-03-11 16:31:41 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TARGET_DIR=~/Documents/blocklist
+FILE_NAME="$(date +"%Y-%m-%d")"
+
+cd "$TARGET_DIR"
+rm domains
+if [ -e "$FILE_NAME" ]; then
+    rm "$FILE_NAME"
+fi
+
+curl --insecure -o "$TARGET_DIR/domains" "$SOURCE_URL"
+
+git add domains
+git commit -m "Update"
+git config user.name "$GIT_USERNAME"
+git config user.email "$GIT_EMAIL"
+git rebase
+git push -u origin main
+
+cd ..
+echo "Script completed successfully."
