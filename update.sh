3f57679d4a140ab93256a7ac344292ecb3d2dabe
Laurensius Jeffrey <7936962+arcestia@users.noreply.github.com>
2024-03-11 19:52:09 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,7 +12,9 @@ curl --insecure -o "domains" "$SOURCE_URL"
 
 git config user.name "Skiddle-Git"
 git config user.email "160487326+Skiddle-Git@users.noreply.github.com"
-git commit -am "Update"
+git add domains
+git commit -m "Update"
+git rebase
 git push -u origin main
 
 cd ..
