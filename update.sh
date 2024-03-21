f7937a978a449a2805c0d38bd82fe178dbba7998
Laurensius Jeffrey <7936962+arcestia@users.noreply.github.com>
2024-03-21 21:29:58 +0700

    Update update.sh
    Signed-off-by: Laurensius Jeffrey <7936962+arcestia@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-FILE_NAME="$(date +"%Y-%m-%d")"
-
-rm domains
-if [ -e "$FILE_NAME" ]; then
-    rm "$FILE_NAME"
-fi
+now=$(date +"%Y-%m-%d_%H-%M-%S")
 
 curl --insecure -o "domains" "$DOMAIN_URL"
 curl --insecure -o "ipaddress_isp" "$IP_URL"
@@ -14,7 +9,7 @@ curl --insecure -o "ipaddress_isp" "$IP_URL"
 git config user.name "Skiddle-Git"
 git config user.email "160487326+Skiddle-Git@users.noreply.github.com"
 git add .
-git commit -m "Update"
+git commit -m "Updated on $now"
 git rebase
 git push -u origin main
 
