138cc8efd83601e9eb76fc85702316b9a8898766
Skiddle Git Assistant <160487326+Skiddle-Git@users.noreply.github.com>
2024-05-05 16:22:42 +0700

    assign to @skiddle-bot
    Signed-off-by: Skiddle Git Assistant <160487326+Skiddle-Git@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ curl --insecure -m 30 -o "ipaddress_isp" "$IP_URL"
 curl --insecure -m 30 -o "situs_judi.txt" "$SITUS_JUDI"
 
 
-git config user.name "Skiddle-Git"
-git config user.email "160487326+Skiddle-Git@users.noreply.github.com"
+git config user.name "skiddle-bot"
+git config user.email "165562787+skiddle-bot@users.noreply.github.com"
 git add .
 git commit -m "Updated on $now"
 git rebase
